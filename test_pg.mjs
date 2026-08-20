baaea6ca75a76048da98d645c36ee826bc73cb80
nftuoa <nftuoa@local>
2026-08-20 18:01:11 +0800

    chore: clean test files, update gitignore

--- test_pg.mjs
+++
@@ -1,5 +0,0 @@
-import pg from 'pg';
-const pool = new pg.Pool({connectionString: process.env.DATABASE_URL, ssl:{rejectUnauthorized:false}});
-const r = await pool.query('select count(*) from survey_responses');
-console.log('pg ok', r.rows[0]);
-await pool.end();
